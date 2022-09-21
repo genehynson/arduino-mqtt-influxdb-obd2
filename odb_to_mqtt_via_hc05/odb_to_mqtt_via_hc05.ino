37d55113197d1a76e92954ce1ec3ae8ad05b4ecc
Gene Hynson <ghynson@influxdata.com>
2022-09-21 15:43:02 -0700

    feat: gps coords

--- odb_to_mqtt_via_hc05/odb_to_mqtt_via_hc05.ino
+++ odb_to_mqtt_via_hc05/odb_to_mqtt_via_hc05.ino
@@ -3,6 +3,7 @@
 #include <ArduinoMqttClient.h>
 #include <TinyGPS++.h>
 #include <utility/wifi_drv.h>
+#include "wiring_private.h"
 
 // elm327
 ELM327 elm;
@@ -12,7 +13,19 @@ float value = -1;
 
 // gps
 TinyGPSPlus gps;
-unsigned long cas = 0;
+const long interval = 200;
+unsigned long previousMillis = 0;
+#define PIN_GPSSERIAL_RX       (1ul)
+#define PIN_GPSSERIAL_TX       (0ul)
+#define PAD_GPSSERIAL_TX       (UART_TX_PAD_0)
+#define PAD_GPSSERIAL_RX       (SERCOM_RX_PAD_1)
+
+Uart gpsSerial(&sercom3, PIN_GPSSERIAL_RX, PIN_GPSSERIAL_TX, PAD_GPSSERIAL_RX, PAD_GPSSERIAL_TX);
+
+void SERCOM3_Handler()
+{
+  gpsSerial.IrqHandler();
+}
 
 // wifi
 char ssid[] = "wifi";
@@ -28,11 +41,15 @@ MqttClient mqttClient(wifiClient);
 // mqtt message
 String topic = "gene/obd";
 String subTopics[20] = {"/rpm","/mph","/engineLoad","/engineCoolantTemp","/intakeAirTemp","/throttle","/fuelLevel","/mafRate","/hybridBatLife","/oilTemp","/emissionRqmts","/demandedTorque","/torque","/batteryVoltage"};
-const long interval = 100;
-unsigned long previousMillis = 0;
 
 void setup()
 {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  pinPeripheral(1, PIO_SERCOM);
+  pinPeripheral(0, PIO_SERCOM);
+  gpsSerial.begin(9600);
+
   // LEDs
   WiFiDrv::pinMode(25, OUTPUT);
   WiFiDrv::pinMode(26, OUTPUT);
@@ -40,11 +57,9 @@ void setup()
   off();
 
   // hc-05 connect to elm327
-  Serial1.begin(9600);
   elm.begin(Serial1, true, 5000);
   
   // wifi connect
-  Serial.begin(9600);
   while (WiFi.status() != WL_CONNECTED) {
     off();
     delay(100);
@@ -76,66 +91,65 @@ void setup()
 
 void loop()
 {
-  unsigned long currentMillis = millis();
   // the elm library is non-blocking and we can only make one query at a time
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    mqttClient.poll();
+  mqttClient.poll();
 
-    // query a msg
-    switch (state) {
-      case 0:
-        value = elm.rpm();
-        break;
-      case 1:
-        value = elm.mph();
-        break;
-      case 2:
-        value = elm.engineLoad();
-        break;
-      case 3:
-        value = elm.engineCoolantTemp();
-        break;
-      case 4:
-        value = elm.intakeAirTemp();
-        break;
-      case 5:
-        value = elm.throttle();
-        break;
-      case 6:
-        value = elm.fuelLevel();
-        break;
-      case 7:
-        value = elm.mafRate();
-        break;
-      case 8:
-        value = elm.hybridBatLife();
-        break;
-      case 9:
-        value = elm.oilTemp();
-        break;
-      case 10:
-        value = (float)elm.emissionRqmts();
-        break;
-      case 11:
-        value = elm.demandedTorque();
-        break;
-      case 12:
-        value = elm.torque();
-        break;
-      case 13:
-        value = elm.batteryVoltage();
-        break;
-      default:
-        state = 0;
-        break;
-    }
-
-    // process the msg recieved
-    processOBD(value, subTopics[state]);
-
-    // processGPS();
+  // query a msg
+  switch (state) {
+    case 0:
+      value = elm.rpm();
+      break;
+    case 1:
+      value = elm.mph();
+      break;
+    case 2:
+      value = elm.engineLoad();
+      break;
+    case 3:
+      value = elm.engineCoolantTemp();
+      break;
+    case 4:
+      value = elm.intakeAirTemp();
+      break;
+    case 5:
+      value = elm.throttle();
+      break;
+    case 6:
+      value = elm.fuelLevel();
+      break;
+    case 7:
+      value = elm.mafRate();
+      break;
+    case 8:
+      value = elm.hybridBatLife();
+      break;
+    case 9:
+      value = elm.oilTemp();
+      break;
+    case 10:
+      value = (float)elm.emissionRqmts();
+      break;
+    case 11:
+      value = elm.demandedTorque();
+      break;
+    case 12:
+      value = elm.torque();
+      break;
+    case 13:
+      value = elm.batteryVoltage();
+      break;
+    default:
+      // reset state and query rpms
+      state = 0;
+      value = elm.rpm();
+      break;
   }
+
+  // process the msg once recieved
+  processOBD(value, subTopics[state]);
+
+  // process latest gps coords
+  processGPS();
 }
 
 void processOBD(float value, String subTopic) {
@@ -159,15 +173,15 @@ void processOBD(float value, String subTopic) {
 void processGPS() {
   double latitude = 0;
   double longitude = 0;
-  while (Serial2.available() > 0){
-      gps.encode(Serial2.read());
-      if (gps.location.isUpdated()){
-        for(millis(); (millis() - cas) > 5000;){
-          cas = millis();
-          latitude = (gps.location.lat());
-          longitude = (gps.location.lng());
-      }
-      String json = "{ \"lat\": \"" + (String)latitude + "\", \"long\": \"" + longitude + "\" }";
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval && gpsSerial.available()) {
+    previousMillis = currentMillis;
+    gps.encode(gpsSerial.read());
+    if (gps.location.isUpdated()) {
+      latitude = gps.location.lat();
+      longitude = gps.location.lng();
+      String json = "{ \"lat\": " + (String)latitude + ", \"long\": " + longitude + " }";
+      Serial.println(json);
       mqttClient.beginMessage(topic + "/gps");
       mqttClient.print(json);
       mqttClient.endMessage();
